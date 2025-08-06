85263999e71b0c29fc38ce14a1954be91aca1951
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2025-08-06 17:00:15 +0300

    add new web Game Tic Tac Toe

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
 // import { createRoot } from "react-dom/client";
-import App from './components/App/App';
+import App from "./components/App/App";
 // 1. Імпортуємо провайдер
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 // 2. Імпортуємо створений раніше стор, який зберігається в файлі redux/filters/store
 // import { store, persistor } from "../src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../src/redux/store";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
@@ -24,4 +23,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
-
